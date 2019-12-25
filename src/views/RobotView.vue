dc5b469cf0b4d8d7bc959c19745fd6c15cc9164b
Octogonapus <firey45@gmail.com>
2019-12-24 21:19:59 -0500

    Add 3d scene

--- src/views/RobotView.vue
+++ src/views/RobotView.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="robotView">
-        <h1>This is the robot view.</h1>
+        <RobotView3DScene class="scene"/>
     </div>
 </template>
 
 <script>
+    import RobotView3DScene from "../components/RobotView3DScene";
+
     export default {
-        name: "RobotView"
+        name: "RobotView",
+        components: {
+            RobotView3DScene
+        }
     }
 </script>
 
 <style scoped>
-
+    .robotView {
+        display: flex;
+        height: 600px;
+    }
 </style>
