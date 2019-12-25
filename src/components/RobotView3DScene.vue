9ba54e3ea7c7d7e95102257089dd0389fa486c46
Octogonapus <firey45@gmail.com>
2019-12-24 22:09:55 -0500

    Test commit

--- src/components/RobotView3DScene.vue
+++ src/components/RobotView3DScene.vue
@@ -38,7 +38,7 @@
             animate: function () {
                 requestAnimationFrame(this.animate);
                 this.mesh.rotation.x += 0.01;
-                this.mesh.rotation.y += 0.02;
+                this.mesh.rotation.y += 0.025;
                 this.renderer.render(this.scene, this.camera);
             }
         },
