a810ed9f9aed44324a26f6451220af32b84b84b9
Octogonapus <firey45@gmail.com>
2019-12-24 20:33:07 -0500

    Initial commit

---
+++ src/views/RobotView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="robotView">
+        <h1>This is the robot view.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RobotView"
+    }
+</script>
+
+<style scoped>
+
+</style>
